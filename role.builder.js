12d97626a4038c1c44297640ad5f52ced97b0515
Steve <srp2120@gmail.com>
2022-11-05 07:29:01 -0700

    fixed builder from building var to working var

--- role.builder.js
+++ role.builder.js
@@ -42,12 +42,12 @@ var roleBuilder = {
             }
         }
 
-        if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
-            creep.memory.building = false;
+        if(creep.memory.working && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.working = false;
             creep.say('🔄 harvest');
         }
-        if(!creep.memory.building && creep.store.getFreeCapacity() == 0) {
-            creep.memory.building = true;
+        if(!creep.memory.working && creep.store.getFreeCapacity() == 0) {
+            creep.memory.working = true;
             creep.say('🚧 build');
         }
 
