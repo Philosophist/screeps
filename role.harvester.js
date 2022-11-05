cd242e5f71566a0e86a30322417944ea8cae98ac
Steve <srp2120@gmail.com>
2022-11-05 07:19:46 -0700

    fixed harvester working = false check and running = false before returns

--- role.harvester.js
+++ role.harvester.js
@@ -4,36 +4,36 @@ var roleHarvester = {
     run: function(creep) {
         creep.memory.running = true;
         var storageRemaining = creep.store.getFreeCapacity();
+        if(creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.working == false;
+        }
         if(creep.memory.working) {
-            if(creep.store[RESOURCE_ENERGY] == 0){
-                creep.memory.working == false;
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                }
+            });
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
             }
             else {
-                var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
-                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-                    }
-                });
+                var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
                 if(targets.length > 0) {
-                    if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                    }
+                    Memory.tarHarvesters -= 1;
+                    creep.memory.role = 'builder';
+                    Memory.tarBuilders += 1;
+                    creep.memory.running = false;
+                    return;
                 }
                 else {
-                    var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-                    if(targets.length > 0) {
-                        Memory.tarHarvesters -= 1;
-                        creep.memory.role = 'builder';
-                        Memory.tarBuilders += 1;
-                        return;
-                    }
-                    else {
-                        Memory.tarHarvesters -= 1;
-                        creep.memory.role = 'upgrader';
-                        Memory.tarUpgraders += 1;
-                        return;
-                    }
+                    Memory.tarHarvesters -= 1;
+                    creep.memory.role = 'upgrader';
+                    Memory.tarUpgraders += 1;
+                    creep.memory.running = false;
+                    return;
                 }
             }
         }
