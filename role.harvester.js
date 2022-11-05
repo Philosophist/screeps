23a2908631f3c22128a347727a5731b8e2398557
Steve <srp2120@gmail.com>
2022-11-04 20:46:52 -0700

    fixed == typo

--- role.harvester.js
+++ role.harvester.js
@@ -37,15 +37,13 @@ var roleHarvester = {
                 }
             }
         }
-        else {
-            if(storageRemaining == 0) {
-                creep.memory.working == true;
-            }
-            else{
-                var sources = creep.room.find(FIND_SOURCES);
-                if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-                }
+        if(storageRemaining == 0) {
+            creep.memory.working = true;
+        }
+        else{
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
         creep.memory.running = false;
